4faa18d4cbf61a13706ffb7627d18aa664b93933
fabio thomaz <fabio_thomaz@live.com>
2024-03-31 11:38:33 -0300

    adicionado vuetify

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+app.use(router)
+app.use(vuetify)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,6 @@
 <template>
   <h1>Home</h1>
+  <v-btn variant="outlined">
+    Button
+  </v-btn>
 </template>
